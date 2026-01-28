f00f1f53275e2a93e62e66b1eaac1044d43e0662
phantom <sahilpatil23347@gmail.com>
2026-01-28 21:44:48 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -229,7 +229,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/+vf02ENon451jNDZl">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/MoviesGroupG3">[ MOVIE ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/+vf02ENon451jNDZl">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/MoviesGroupG3">[ ASH MOVIES ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
@@ -404,7 +404,7 @@ sʜᴏᴡɴ ɪɴ : {remaining_seconds} <i>sᴇᴄ</i>⚡️
     SOURCE_TXT ="""<b>ՏOᑌᖇᑕᗴ ᑕOᗪᗴ : 👇 </b>
 
 This Is An Open-Source Project. You Can Use It Freely, But Selling The Source Code Is Strictly Prohibited.\n
-
+ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʜᴇʀᴇ ◉› :<a href=https://github.com/DreamXBotz/Auto_Filter_Bot.git>𝓓𝓻𝓮𝓪𝓶𝔁𝓑𝓸𝓽𝔃</a>\n """
 
     SETTING_TXT = """    
 <u>ꜱᴇᴛᴛɪɴɢꜱ</u> :
